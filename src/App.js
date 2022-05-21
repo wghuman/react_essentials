4ad3b52a246b7b32c525d6098d8def162fbe0632
wghuman <ghumanz@gmail.com>
2022-05-22 00:53:33 +0200

    Practicing conditional rendering using fragments

--- src/App.js
+++ src/App.js
@@ -1,66 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import burgerImage from "./burger.svg";
 
 
-
-
-
-function Header(props){
-  return(
-<header>
-<h1> {props.name} Sverige </h1>
-</header>
-  );
+function SecretComponent() {
+  return <h1>Super secret information only for the authorized users</h1>;
 }
 
-function Main(props){
-  
-  return(
-    <section>
-      <p> We deliver the products with {props.adjective} and service in mind</p>
-      <img src = {burgerImage} height={200}/>
-      <ul>
-      {props.services.map((service) => 
-      <li key={service.id}>
-        {service.title}
-
-      </li>)}
-      </ul>
-    </section>
-  );
+function RegularComponent() {
+  return <h1>Regular information for all the users</h1>;
 }
-
-function Footer(props){
-  return(
-    <footer>Copyrights {props.year}</footer>
-  );
-}
-
-const services = [
-  "Home Delivery",
-  "Home Delivery and montering",
-  "Home Delivery, montering and emblage",
-  "Home Delivyer, montering, emblage and old furniture",
-  "and more",
-  "what else"
-]
-
-const serviceObjects = services.map((service, i) => ({id: i, title: service})) 
-//console.log(serviceObjects);
-
-//services.map((service) => console.log(service));
-
-
-//services.map( (service) => console.log(service))
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <Header name="Mittra"/>
-      <Main adjective="quality" services={serviceObjects}/>
-      <Footer year = {new Date().getFullYear()}/>
-    </div>
-  );
+    <>
+      {props.autohrized ? <SecretComponent /> : <RegularComponent />}
+    </>
+  )
 }
 
 export default App;
